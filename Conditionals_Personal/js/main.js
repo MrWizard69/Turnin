cea0cf8d69d244dd2f75b4d4064368141a33e3ac
Jordan Wilson <JordanWilson@fullsail.edu>
2013-07-25 15:11:31 -0400

    Personal conditionals 4

--- Conditionals_Personal/js/main.js
+++ Conditionals_Personal/js/main.js
@@ -2,11 +2,12 @@
 // Personal conditions
 // 7-25-13
 
-var sprout = prompt("Say you are started a new garden, how tall in cm is your new little sprout?");
+var sprout1 = prompt("Say you are started a new garden, how tall in cm is your new little sprout?");
 
-if(sprout == ""){
+if(sprout1 == ""){
     console.log("I don't think you typed anything in...");
 }
 else{
-    console.log("yay!")
+    var sprout2 = prompt("At the end of the first month how tall in cm is your plant now?");
 }
+
