6441260fbfe03814e9e811ffb0b6ca63a426fc5f
Jordan Wilson <JordanWilson@fullsail.edu>
2013-07-25 17:29:51 -0400

    wacky conditions 3

--- Conditionals- Wacky/js/main.js
+++ Conditionals- Wacky/js/main.js
@@ -5,5 +5,13 @@
 var dist = prompt("In minecraft, your safe house is how many blocks away?");
 
 if(dist == ""){
-    console.log("TYPE SOMETHING!!!")
+    console.log("TYPE SOMETHING!!!");
 }
+else{
+    var aday = prompt("How many blocks can you travel a day?");
+}
+
+if(aday == ""){
+    console.log("No information for you!")
+}
+else{}
