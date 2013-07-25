ced4f6de7bdbeaabd00be38bdeaefa2baf65e3ec
Jordan Wilson <JordanWilson@fullsail.edu>
2013-07-25 15:04:29 -0400

    no message

--- Conditionals_Personal/js/main.js
+++ Conditionals_Personal/js/main.js
@@ -2,4 +2,11 @@
 // Personal conditions
 // 7-25-13
 
+var sprout = prompt("Say you are started a new garden, how tall in cm is your new little sprout?");
 
+if(sprout == ""){
+    console.log("I don't think you typed anything in");
+}
+else{
+    console.log("yay!")
+}
