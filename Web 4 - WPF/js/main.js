5775d3392aebc93b4143f77ffc298a868fe566ad
Jordan Wilson <JordanWilson@fullsail.edu>
2013-07-31 12:00:57 -0400

    reorganizing

---
+++ Web 4 - WPF/js/main.js
@@ -0,0 +1,17 @@
+// Functions
+
+function helloMsg(){
+    console.log("Hello World! :D");
+}
+
+function status(){
+    console.log("Looks like I\'ll be using a boilerplate this time...");
+}
+
+function whtThe(){
+    console.log("What the F???");
+}
+
+helloMsg();
+status();
+whtThe();
