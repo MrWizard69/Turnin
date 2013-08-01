094aec3369973e3b7e31ccf61f8d84bc032e9d67
Jordan Wilson <JordanWilson@fullsail.edu>
2013-08-01 19:02:14 -0400

    functions industry 2

--- Functions_Industry/js/main.js
+++ Functions_Industry/js/main.js
@@ -1,7 +1,3 @@
-/**
- * Created with JetBrains WebStorm.
- * User: jordanwilson
- * Date: 8/1/13
- * Time: 6:59 PM
- * To change this template use File | Settings | File Templates.
- */
+//Jordan Wilson
+//8-1-13
+//Functions Industry
