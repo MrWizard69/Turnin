ce1d56f051191b36c142bb87139c3a46ad6f6bbb
Jordan Wilson <JordanWilson@fullsail.edu>
2013-08-01 18:32:29 -0400

    functions personal 4

--- Functions_Personal/js/main.js
+++ Functions_Personal/js/main.js
@@ -4,5 +4,11 @@
 
 var tree1 = prompt("You find two trees in the forrest. You want to see which tree is older. What is the diameter of the first tree in inches?");
 
+if(tree1 == ""){
+    console.log("Type something in!");
+}
+else{
 
+    var tree2 = prompt("What is the diameter of the second tree in inches?");
+}
 
