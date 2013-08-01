27e7df71866e9ced0f0cb77e1326f34ba1949c09
Jordan Wilson <JordanWilson@fullsail.edu>
2013-08-01 17:35:31 -0400

    Functions Personal 2

--- Functions_Personal/js/main.js
+++ Functions_Personal/js/main.js
@@ -1,7 +1,5 @@
-/**
- * Created with JetBrains WebStorm.
- * User: jordanwilson
- * Date: 8/1/13
- * Time: 5:32 PM
- * To change this template use File | Settings | File Templates.
- */
+//Jordan Wilson
+//8-1-13
+//Functions- Personal
+
+
