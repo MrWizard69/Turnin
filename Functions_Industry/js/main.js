6bf1d6840fce775179465ae5b2a06f99cce00e4a
Jordan Wilson <JordanWilson@fullsail.edu>
2013-08-01 19:53:04 -0400

    functions industry 5

--- Functions_Industry/js/main.js
+++ Functions_Industry/js/main.js
@@ -7,4 +7,7 @@ var houseL = prompt("So you want to build a house? What is the length in yards?"
 if(houseL == ""){
     console.log("You no types nothing in!");
 }
+else{
+    var houseW = prompt("What is the Width of this house in yards?");
+}
 
