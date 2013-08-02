71e2d2327b225e96bd921b81e5ffddd1b4defdf6
Jordan Wilson <JordanWilson@fullsail.edu>
2013-08-01 21:08:51 -0400

    functions wacky 17

--- Functions_Wacky/js/main.js
+++ Functions_Wacky/js/main.js
@@ -11,8 +11,8 @@ var ranNum = Math.ceil(Math.random() * 10 + 1); //the random number the user mus
 while(guess != ranNum){// while the users input doesn't equal the random number...
     var guess = prompt("Guess a number between 1 and 10"); // gets the users input
     if(guess < ranNum){// if the users guess is less than the random number
-        console.log("Sorry, your guess is too low");
-        guess = "";
+        console.log("Sorry, your guess is too low"); //the user gets a message telling them that they guessed too low
+        guess = ""; //clears the guess variable
     }
     else if(guess > ranNum){
         console.log("Sorry, your guess is too high");
